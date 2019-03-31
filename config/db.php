602ef4d1c1cb54ee9500df8a2e15495fc5ceb55a
xintaiideas <1948188829@qq.com>
2019-03-31 17:26:51 +0800

    rbac password 123456

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=rbac',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '123456',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
